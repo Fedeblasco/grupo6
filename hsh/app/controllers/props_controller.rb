371ff967869d8d61c5cdee3e783037a298042e6e
Martin Paradiso <martinparadiso@protonmail.com>
2018-11-04 22:07:59 -0300

    Agregada pagina principal, con link a lista de propiedades

--- hsh/app/controllers/props_controller.rb
+++ hsh/app/controllers/props_controller.rb
@@ -1,2 +1,26 @@
 class PropsController < ApplicationController
+
+  def new
+    @prop = Prop.new
+  end
+
+  def create
+    @prop = Prop.create(
+      params.requiere(:prop).permit(:nombre, :ubicacion)
+      )
+
+    if @prop.save
+      redirect_to @prop
+    else
+      redirect_to :new
+    end
+  end
+
+  def show
+    @prop = Porp.find(param(:id))
+  end
+  
+  def index
+    @props = Prop.all
+  end
 end
