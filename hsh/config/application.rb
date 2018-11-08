a493f374f3401b536d5d4c94198e9ca8a5931498
Gauthier P <gauthiernpn@gmail.com>
2018-11-08 03:04:00 -0300

    Añadida la gestión de idioma

--- hsh/config/application.rb
+++ hsh/config/application.rb
@@ -10,6 +10,8 @@ module Hsh
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en, :fr]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
