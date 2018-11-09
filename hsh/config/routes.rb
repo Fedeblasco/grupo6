fba5004aba655646f00bd0f2ede080cb536e4259
Gauthier P <gauthiernpn@gmail.com>
2018-11-09 09:27:33 -0300

    Añadida traduccion y modificando la interfaz

--- hsh/config/routes.rb
+++ hsh/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
   resources :props
 
-  root 'main#index'
+  root 'props#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
