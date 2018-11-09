9e4dff69b7596b85772b5e112c6cf8aaa36fc2d9
Martin Paradiso <martinparadiso@protonmail.com>
2018-11-08 23:34:58 -0300

    Creada la vista para la creacion de usuarios

--- hsh/app/controllers/usuarios_controller.rb
+++ hsh/app/controllers/usuarios_controller.rb
@@ -5,9 +5,23 @@ class UsuariosController < ApplicationController
   end
 
   def new
+  	@usuario = Usuario.new
+  end
+
+  def create
+
+  	@usuario = Usuario.create(params.require(:usuario).permit(:mail))
+
+  	if @usuario.save
+  		redirect_to @usuario
+  	else
+  		render :new
+  	end
+
   end
 
   def show
   	@usuario = Usuario.find(params[:id])
   end
+
 end
