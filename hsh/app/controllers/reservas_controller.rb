89f0b6689159c0180d49dce2feb1d5838ee0d4d2
Martin Paradiso <martinparadiso@protonmail.com>
2018-11-09 00:20:45 -0300

    Se pueden agregar reservas mediante interfaz.
    Se agrego el formulario para crear reserva, tiene dropdown para elegir usuarios y propiedades existentes.
    
    Se modifico la busqueda para que busque solo por ubicacion O  nombre

--- hsh/app/controllers/reservas_controller.rb
+++ hsh/app/controllers/reservas_controller.rb
@@ -5,9 +5,20 @@ class ReservasController < ApplicationController
   end
 
   def new
+  	@reserva = Reserva.new
   end
 
   def create
+
+  	@reserva = Reserva.create(params.require(:reserva).permit(:fecha, :usuario_id, :prop_id))
+
+  	#@reserva.fecha = @reserva.fecha - @reserva.fecha.wday
+
+  	if @reserva.save
+  		redirect_to reservas_path
+  	else
+  		render :new
+  	end
   end
 
 end
