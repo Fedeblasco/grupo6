b77d9d8966cf91e104e17c76aa1f5581da7b4487
Martin Paradiso <martinparadiso@protonmail.com>
2018-11-10 22:08:10 -0300

    Acomodado para que vuelva a andar

--- hsh/config/routes.rb
+++ hsh/config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :props
   resources :subs
 
-  root 'props#index'
+  root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- hsh/db/schema.rb
+++ hsh/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_08_043924) do
-=======
 ActiveRecord::Schema.define(version: 2018_11_11_002534) do
->>>>>>> agregar-fechas
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
