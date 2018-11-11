5c334061114205980c308e0ff1116a63c36a76f2
Martin Paradiso <martinparadiso@protonmail.com>
2018-11-10 21:03:18 -0300

    Sacada tabla users

--- hsh/db/schema.rb
+++ hsh/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_202254) do
+ActiveRecord::Schema.define(version: 2018_11_10_235938) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,18 +58,6 @@ ActiveRecord::Schema.define(version: 2018_11_10_202254) do
     t.datetime "updated_at", null: false
     t.integer "prop_id"
     t.index ["prop_id"], name: "index_subs_on_prop_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "usuarios", force: :cascade do |t|
