489d2227ffa1082266388af6da0a435e94b9af65
Fedeblasco <fedeblasco@hotmail.com>
2018-11-14 05:08:39 -0300

    Pujas funcionando con listado y creacion

--- app/controllers/pujas_controller.rb
+++ app/controllers/pujas_controller.rb
@@ -1,14 +1,21 @@
-class ReservasController < ApplicationController
+class PujasController < ApplicationController
+
+  def new
+    @puja = Puja.new
+  end
 
   def index
   	@pujas = Puja.all
   end
 
-  def new
-  	@puja = Puja.new
-  end
-
   def create
+    @puja = Puja.new(params.require(:puja).permit(:valor,:usuario_id,:sub_id))
+    if @puja.save
+      redirect_to pujas_path
+    else
+      flash[:alert] = "Error al guardar"
+      render :new
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :reservas
   resources :props
   resources :subs
+  resources :pujas
 
   root 'props#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
