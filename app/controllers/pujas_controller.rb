e069b92c948cf55895b8c593f2456c23e5b1e2e0
Martin Paradiso <martinparadiso@protonmail.com>
2018-11-14 20:30:28 -0300

    Al pujar redirecciona a la subasta

--- app/controllers/pujas_controller.rb
+++ app/controllers/pujas_controller.rb
@@ -4,14 +4,10 @@ class PujasController < ApplicationController
     @puja = Puja.new(sub_id: params[:id])
   end
 
-  def index
-  	@pujas = Puja.all
-  end
-
   def create
     @puja = Puja.new(params.require(:puja).permit(:valor,:usuario_id,:sub_id))
     if @puja.save
-      redirect_to pujas_path
+      redirect_to sub_path(@puja.sub_id)
     else
       flash[:alert] = "Error al pujar"
       redirect_to pujar_path(params[:sub_id])
