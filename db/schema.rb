9fa5506ef953ae0afe23b5401973d54797856b54
Gauthier PONTET <gauthiernpn@gmail.com>
2018-11-18 20:58:45 -0300

    fix usuarios table

--- db/schema.rb
+++ db/schema.rb
@@ -99,13 +99,14 @@ ActiveRecord::Schema.define(version: 2018_11_17_183607) do
   end
 
   create_table "usuarios", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
