7ac82a9dd45169c711ae5fee0476642bdd4122db
Gauthier PONTET <gauthiernpn@gmail.com>
2018-11-19 01:38:19 -0300

    Edición de páginas de inicio de sesión

--- db/schema.rb
+++ db/schema.rb
@@ -99,9 +99,9 @@ ActiveRecord::Schema.define(version: 2018_11_17_183607) do
   end
 
   create_table "usuarios", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
