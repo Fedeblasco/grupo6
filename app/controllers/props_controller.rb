ec253d44fe0d39350c1f6dc9f0b0be304c4f1e07
Fedeblasco <fedeblasco@hotmail.com>
2018-11-29 23:26:44 -0300

    Correcion error en el listado de propiedades

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -67,74 +67,74 @@ class PropsController < ApplicationController
   end
 
   def index
-    if admin_signed_in? || usuario_signed_in?
+      if admin_signed_in? || usuario_signed_in?
 
-      # Carga las propiedades a mostrar, si se pasaron opciones de busqueda filtra, si no devuelve todo
-      @props = params[:busq] ? Prop.includes(:hot_sale).order('hot_sales.fecha_hotsale ASC, nombre').where('(ubicacion LIKE ?) OR (nombre LIKE ?)', "%#{params[:busq]}%", "%#{params[:busq]}%") : Prop.includes(:hot_sale).order('hot_sales.fecha_hotsale ASC, nombre').all
+        # Carga las propiedades a mostrar, si se pasaron opciones de busqueda filtra, si no devuelve todo
+        @props = params[:busq] ? Prop.includes(:hot_sale).order('hot_sales.fecha_hotsale ASC, nombre').where('(ubicacion LIKE ?) OR (nombre LIKE ?)', "%#{params[:busq]}%", "%#{params[:busq]}%") : Prop.includes(:hot_sale).order('hot_sales.fecha_hotsale ASC, nombre').all
 
-      # Archivo donde guarda temporalmente las propiedades que tiene que eliminar
-      remove = []
+        # Archivo donde guarda temporalmente las propiedades que tiene que eliminar
+        remove = []
 
-    
-      # Si no soy admin y la propiedad esta oculta, no la tengo en cuenta
-      @props.each do |p|
-          if not admin_signed_in?
-            if p.oculto
-              remove << p
-            end
-          end
-      end
-
-      # Si se enviaron los parametros de fecha
-      if (params[:desde] && params[:hasta]) && (params[:desde] != "" && params[:hasta] != "")
-        # Convierte el parametro que llego como string a tipo Date
-        desde = Date.parse(params[:desde])
-        hasta = Date.parse(params[:hasta])
-
-        # Fuerza las fechas a que sean domingo y sabado
-        desde = desde - desde.wday
-        hasta = hasta + 7
-        hasta = hasta - hasta.wday - 1
-
-        # Calcula la cantidad de semanas en el intervalo
-        diff = ((hasta - desde + 1).to_i / 7 )
-
-        # Si las fechas tienen coherencia (desde es anterior a hasta, y el intervalo no es mayor a 2 meses)
-        if (desde < hasta)
-          if ( ( diff ) <= 8)
-
-            # Recorre todas las propiedades para filtrar las que tienen semanas libres
-            @props.each do |p|
-
-              # Si la cantidad de reservas es igual a la cantidad de semanas en el
-              # intervalo, significa que no hay semanas libres 
-              if (p.reserva.where(" (fecha >= ?) AND ( fecha < ?) ", desde, hasta ).count >= diff)
+      
+        # Si no soy admin y la propiedad esta oculta, no la tengo en cuenta
+        @props.each do |p|
+            if not admin_signed_in?
+              if p.oculto
                 remove << p
               end
-
             end
-
-            # Actualiza los parametros para que la fecha a mostrar sea la fecha corregida.
-            # Para la semana de inicio se indica el domingo de la primer semana
-            params[:desde] = desde
-            # Para la ultima semana se indica el sabado que se deberia dejar la propiedad
-            params[:hasta] = hasta 
-
-            flash.now[:notice] = t('.cant_semanas') + "#{diff}"
-          else
-            flash.now[:alert] = t('.error_intervalo')
-          end
-        else
-          # Avisa que las semanas no tienen sentido
-          flash.now[:alert] = t('props.search.date_error');
         end
+
+        # Si se enviaron los parametros de fecha
+        if (params[:desde] && params[:hasta]) && (params[:desde] != "" && params[:hasta] != "")
+          # Convierte el parametro que llego como string a tipo Date
+          desde = Date.parse(params[:desde])
+          hasta = Date.parse(params[:hasta])
+
+          # Fuerza las fechas a que sean domingo y sabado
+          desde = desde - desde.wday
+          hasta = hasta + 7
+          hasta = hasta - hasta.wday - 1
+
+          # Calcula la cantidad de semanas en el intervalo
+          diff = ((hasta - desde + 1).to_i / 7 )
+
+          # Si las fechas tienen coherencia (desde es anterior a hasta, y el intervalo no es mayor a 2 meses)
+          if (desde < hasta)
+            if ( ( diff ) <= 8)
+
+              # Recorre todas las propiedades para filtrar las que tienen semanas libres
+              @props.each do |p|
+
+                # Si la cantidad de reservas es igual a la cantidad de semanas en el
+                # intervalo, significa que no hay semanas libres 
+                if (p.reserva.where(" (fecha >= ?) AND ( fecha < ?) ", desde, hasta ).count >= diff)
+                  remove << p
+                end
+
+              end
+
+              # Actualiza los parametros para que la fecha a mostrar sea la fecha corregida.
+              # Para la semana de inicio se indica el domingo de la primer semana
+              params[:desde] = desde
+              # Para la ultima semana se indica el sabado que se deberia dejar la propiedad
+              params[:hasta] = hasta 
+
+              flash.now[:notice] = t('.cant_semanas') + "#{diff}"
+            else
+              flash.now[:alert] = t('.error_intervalo')
+            end
+          else
+            # Avisa que las semanas no tienen sentido
+            flash.now[:alert] = t('props.search.date_error');
+          end
+        end
+        # Elimina las reservas 
+        @props = @props - remove
+      else
+        flash[:alert] = t('forbidden')
+        redirect_to prohibido_path
       end
-      # Elimina las reservas 
-      @props = @props - remove
     end
-  else
-      flash[:alert] = t('forbidden')
-      redirect_to prohibido_path
-  end
 end
 
